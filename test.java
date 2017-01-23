ecb1b50b7c244cc24098ea0b24e6366938bca8fd
jeongminP <pjm9355@naver.com>
2017-01-23 18:13:28 +0900

    working at javac

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+	public static void main(String[] args){
+
+		String[] words={"funk","chunk","furry","baconantor"};
+
+		for(String w:words){
+			if(w.startsWith("fu"))
+				System.out.println(w+" starts with fu");
+		}
+		for(String w:words){
+			if(w.endsWith("nk"))
+				System.out.println(w+" ends with nk");
+		}
+	}
+}
